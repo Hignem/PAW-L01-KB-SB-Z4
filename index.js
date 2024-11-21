310ff81ed7a833ed1655a5f6ac05ec4b5487a4a3
Hignem <81192237+Hignem@users.noreply.github.com>
2024-11-21 09:48:36 +0100

    4_2.1

--- index.js
+++ index.js
@@ -55,7 +55,6 @@ app.get("/math/power/:base/:exponent", (req, res) => {
 
   const powerResult = Math.pow(base, exponent).toFixed(2);
   if (root) {
-    // Jeśli root = true, oblicz również pierwiastek kwadratowy
     if (base < 0) {
       return res.status(400).send({
         error:
@@ -63,7 +62,7 @@ app.get("/math/power/:base/:exponent", (req, res) => {
       });
     }
 
-    const sqrtResult = Math.sqrt(base).toFixed(2); // Oblicz pierwiastek kwadratowy
+    const sqrtResult = Math.sqrt(base).toFixed(2);
     return res.send({
       base: base,
       exponent: exponent,
@@ -72,12 +71,61 @@ app.get("/math/power/:base/:exponent", (req, res) => {
     });
   }
 
-  // Jeśli root = false, zwróć tylko wynik potęgowania
   res.send({
     base: base,
     exponent: exponent,
     powerResult: powerResult,
   });
+});
+
+let categories = ["funnyJoke", "lameJoke"];
+
+let funnyJoke = [
+  {
+    joke: "Dlaczego komputer poszedł do lekarza?",
+    response: "Bo złapał wirusa!",
+  },
+  {
+    joke: "Dlaczego komputer nie może być głodny?",
+    response: "Bo ma pełen dysk!",
+  },
+  {
+    joke: "Co mówi jeden bit do drugiego?",
+    response: "„Trzymaj się, zaraz się przestawiamy!”",
+  },
+];
+
+let lameJoke = [
+  {
+    joke: "Dlaczego programiści preferują noc?",
+    response: "Bo w nocy jest mniej bugów do łapania!",
+  },
+  {
+    joke: "Jak nazywa się bardzo szybki programista?",
+    response: "Błyskawiczny kompilator!",
+  },
+];
+
+const jokesMap = {
+  funnyJoke: funnyJoke,
+  lameJoke: lameJoke,
+};
+
+app.get("/jokebook/categories", (req, res) => {
+  res.send({ categories });
+});
+
+app.get("/jokebook/joke/:category", (req, res) => {
+  const category = req.params.category;
+
+  if (!jokesMap[category]) {
+    return res.status(404).send({ error: `no jokes for category ${category}` });
+  }
+
+  const jokes = jokesMap[category];
+  const randomJoke = jokes[Math.floor(Math.random() * jokes.length)];
+
+  res.send(randomJoke);
 });
 
 const PORT = process.env.PORT || 3000;
